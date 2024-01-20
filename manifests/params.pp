dd2dbfa2e2fabb3ffae76f8205cfb016afd68c01
root <root@puppet-master.us-west1-b.c.non-prod-gaurav-maheshwari.internal>
2024-01-20 01:34:23 +0000

    Refactored install class for Debian Family servers

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+# @summary 
+# Operating System-related variables (and others)
+#
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+       $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+      }
+  }
+
+}
